886cbf474e03048ac721b5f2594837893c18daa1
llanse01 <65236076+llanse01@users.noreply.github.com>
2020-07-14 23:13:10 -0400

    Update bootstrap2.sh

--- aks-bootstrap-flux/bootstrap2.sh
+++ aks-bootstrap-flux/bootstrap2.sh
@@ -14,6 +14,8 @@ helm repo update
 
 echo "Installing Flux..."
 
+known_host=`echo "\"$(ssh-keyscan -t rsa -p "$(echo "${config_repo_url}" | sed -e "s/\([^@]*\)\@\([^:]*\):\([0-9]*\).*/\3/" | sed -e "s/^$/22/")" "$(echo "${config_repo_url}" | sed -e "s/\([^@]*\)\@\([^:]*\).*/\2/")")"\"`
+
 kubectl apply --filename https://raw.githubusercontent.com/fluxcd/helm-operator/v1.1.0/deploy/crds.yaml
 
 kubectl create namespace fluxcd
@@ -56,7 +58,7 @@ git:
   path: ${config_repo_path}
   secretName: flux-ssh
 ssh:
-  known_hosts: 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
+  known_hosts: ${known_host}
   enabled: true
 extraVolumes:
  - name: git-keygen2
